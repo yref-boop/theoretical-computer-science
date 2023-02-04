5ea08371fb86e8914ca2c2a1a987ab1cfc70e6f8
yref-boop <regofdez.yago@gmail.com>
2023-02-04 22:39:18 +0100

    wip: added conjuntos function definitions

---
+++ p1/conjuntos.ml
@@ -0,0 +1,28 @@
+(*definicion de tipo dada*)
+type 'a conjunto = Conjunto of 'a list
+;;
+
+let pertenece item set =
+    Conjunto a
+
+let agregar item set =
+
+let conjunto_of_list list =
+
+let suprimir item set =
+
+let cardinal set =
+
+let union set1 set2 =
+
+let interseccion set1 set2 =
+
+let diferencia set1 set2 =
+
+let incluido set1 set2 =
+
+let igual set1 set2 =
+
+let producto_cartesiano set1 set2 =
+
+let list_of_conjunto set =
