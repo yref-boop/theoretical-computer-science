fd23567443f493ab5aa21e3855e2cdd1cba32510
yref-boop <regofdez.yago@gmail.com>
2023-02-06 17:43:18 +0100

    refactor: simplified tree functions

--- p1/tree.ml
+++ p1/tree.ml
@@ -9,57 +9,35 @@ let t = Nodo (3, Nodo (2, Vacio, Vacio), Nodo (5, Nodo (4, Vacio, Vacio), Nodo (
 (*ejemplo extra*)
 let t = Nodo (3, Nodo (2, Nodo (9, Vacio, Vacio), Vacio), Nodo (5, Nodo (4, Vacio, Vacio), Nodo (1, Vacio, Vacio)));;
 
-(*funcion auxiliar:*)
-let rec fold_tree f accum = function
-    | Vacio -> accum
-    | Nodo (root, left, right) ->
-        f root (fold_tree f accum left) (fold_tree f accum right);;
-
-let in_order tree =
-    fold_tree (fun root left right -> left @ root :: right) [] tree;;
-
-let pre_order tree =
-    fold_tree (fun root left right -> root :: left @ right) [] tree;;
-
-let post_order tree =
-    fold_tree (fun root left right -> left @ right @ [root]) [] tree;;
-
-
-let in_order tree =
-    let rec aux tree accumulator = match tree with
-        | Vacio -> accumulator
-        | Nodo (value, left, right) ->
-            let acc_with_right = aux right accumulator in
-            let acc_with_value = value :: acc_with_right in
-            aux left acc_with_value in
-    aux tree []
+let in_orden tree =
+    let rec fold accum = function
+        | Vacio -> accum
+        | Nodo (root, left, right) ->
+            fold (root :: (fold accum right)) left
+    in fold [] tree
 ;;
 
-let pre_order tree =
-    let rec aux tree accumulator = match tree with
-        | Vacio -> accumulator
-        | Nodo (value, left, right) ->
-            let acc_with_right = aux right accumulator in
-            let acc_with_left = aux left acc_with_right in
-            value :: acc_with_left in
-    aux tree []
+let pre_orden tree =
+    let rec fold accum = function
+        | Vacio -> accum
+        | Nodo (root, left, right) ->
+            root :: (fold (fold accum right) left)
+    in fold [] tree
 ;;
 
-let post_order tree =
-    let rec aux tree accumulator = match tree with
-        | Vacio -> accumulator
-        | Nodo (value, left, right) ->
-            let acc_with_value = value :: accumulator in
-            let acc_with_right = aux right acc_with_value in
-            aux left acc_with_right in
-    aux tree []
+let post_orden tree =
+    let rec fold accum = function
+        | Vacio -> accum
+        | Nodo (root, left, right) ->
+            fold (fold (root :: accum) right) left
+    in fold [] tree
 ;;
 
 let anchura tree =
-    let rec aux accum results = match accum with
-        | [] -> List.rev results
-        | Vacio :: t -> aux t results
-        | (Nodo (root, left, right)) :: t ->
-            aux (List.rev (right :: left :: List.rev t)) (root :: results)
-    in aux [tree] []
+    let rec fold result = function
+        | [] -> List.rev result
+        | Vacio :: t -> fold result t
+        | Nodo (root, left, right) :: t ->
+            fold (root :: result) (List.rev (right :: left :: List.rev t))
+    in fold [] [tree]
 ;;
