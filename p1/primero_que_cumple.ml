dd787a6c8cc42237c809638baec3f0b877e6e75b
yref-boop <regofdez.yago@gmail.com>
2023-02-12 20:13:41 +0100

    feat: added exception on empty list

--- p1/primero_que_cumple.ml
+++ p1/primero_que_cumple.ml
@@ -1,8 +1,12 @@
 (*dado un predicado ('a -> bool) -> ['a] -> 'a*)
 (*primero_que_cumple : ('a -> bool) -> ['a] -> 'a*)
 let rec primero_que_cumple predicado list = match list with
-    | [] -> raise (Not_found)
-    | h :: t -> if (predicado h) then h else primero_que_cumple predicado t
+    | [] -> raise (Invalid_argument "list")
+    | _ ->
+        let rec aux predicado = function
+            | [] -> raise (Not_found)
+            | h :: t -> if (predicado h) then h else aux predicado t
+        in aux predicado list
 ;;
 
 (* existe : ('a -> bool ) -> a' list -> bool*)
@@ -11,21 +15,20 @@ let rec primero_que_cumple predicado list = match list with
 let rec existe predicado list =
     let check n = true in
     try check (primero_que_cumple predicado list) with
-        Not_found -> false
+        | Not_found -> false
+        | Invalid_argument _ -> raise (Invalid_argument "list")
 ;;
 
 (*lista de pares (clave, valor)*)
 (*dado conjunto y clave, devuelve valor*)
 let asociado lista valor =
-    let comprobar valor n = valor == n in
-    primero_que_cumple (comprobar valor) lista
+    let comprobar valor n = valor == n
+    in primero_que_cumple (comprobar valor) lista
 ;;
-
-(*funcion auxiliar de asociado*)
-let comprobar valor n = valor == (fst n) ;;
 
 (*lista de pares (clave, valor)*)
 (*dado conjunto y clave, devuelve valor*)
 let asociado lista valor =
-    snd (primero_que_cumple (comprobar valor) lista)
+    let comprobar valor n = valor == (fst n)
+    in snd (primero_que_cumple (comprobar valor) lista)
 ;;
